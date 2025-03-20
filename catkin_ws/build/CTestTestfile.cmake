8f635448e50cf62d843392ced73cbe5e35bccc16
cardonixjr <cardoinxjr@gmail.com>
2025-03-20 15:30:45 -0300

    add RPlidar and teleop

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/luciano/catkin_ws/src
+# Build directory: /home/luciano/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("teleop_twist_keyboard")
+subdirs("rplidar_ros")
+subdirs("MD49")
