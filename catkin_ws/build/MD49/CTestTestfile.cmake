77c26ffd24a6a3e776e75bc1e663aa9e55a9c182
cardonixjr <cardoinxjr@gmail.com>
2025-03-26 17:26:06 -0300

    reorganizing the repository

--- catkin_ws/build/MD49/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luciano/catkin_ws/src/MD49
-# Build directory: /home/luciano/catkin_ws/build/MD49
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
